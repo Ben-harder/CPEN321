efe9ab5d1cfe2bd65679ab6673f22665ba081c76
Ben <ben.harder2@gmail.com>
2018-10-21 16:45:51 -0700

    added job and joblist gets

--- backend/api/router.js
+++ backend/api/router.js
@@ -13,4 +13,15 @@ module.exports = function(app) {
   app.get('/user-exists', (req, res) => {
     return res.status(200).json({ phoneNumber: req.query.phoneNumber });
   });
+
+  app.post('/job', (req, res) => {
+    console.log(req.body);
+  });
+  
+  app.get('/jobList', (req, res) => {
+    res.send(
+      [{key: 'wain1'},
+      {key: 'wain2'}]
+    );
+  });
 }
