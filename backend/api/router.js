2b44203c0afc09e8953c28ba8baa4f7a72731990
Randy Jama <randyjama@server.ca>
2018-10-21 18:19:02 -0700

    wain

--- backend/api/router.js
+++ backend/api/router.js
@@ -1,4 +1,5 @@
 import { app } from '../server';
+import SignupService from './auth/signup';
 
 module.exports = function(app) {
   app.get('/hello', (req, res) =>
@@ -7,10 +8,12 @@ module.exports = function(app) {
 
   app.post('/create-user', (req, res) => {
     console.log(req.body);
+    SignupService.createNewUser(req.body); // must have error message handling based on osama's retval.isErr (error is another var in obj)
     return res.sendStatus(200);
   });
 
   app.get('/user-exists', (req, res) => {
     return res.status(200).json({ phoneNumber: req.query.phoneNumber });
   });
+
 }
