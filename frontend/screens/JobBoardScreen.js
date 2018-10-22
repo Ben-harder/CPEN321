3de709c855b8fcf1fd7ceebb89fb1a70664affe8
Ben <ben.harder2@gmail.com>
2018-10-21 19:38:00 -0700

    added job screen

--- frontend/screens/JobBoardScreen.js
+++ frontend/screens/JobBoardScreen.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  Button,
-  AsyncStorage,
-  FlatList,
+import
+{
+    Image,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+    Button,
+    AsyncStorage,
+    FlatList,
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { WebBrowser } from 'expo';
@@ -18,12 +19,14 @@ import api from "../constants/Url";
 
 import { MonoText } from '../components/StyledText';
 
-export default class JobBoardScreen extends React.Component {
+export default class JobBoardScreen extends React.Component
+{
     static navigationOptions = {
         header: null,
     };
 
-    constructor(props) {
+    constructor(props)
+    {
         super(props);
         this.state = {
             jobList: [],
@@ -32,46 +35,73 @@ export default class JobBoardScreen extends React.Component {
         this.tryFetchJobList = this.tryFetchJobList.bind(this);
     }
 
-    componentDidMount() {
+    componentDidMount()
+    {
         this.tryFetchJobList();
         this.jobInterval = setInterval(this.tryFetchJobList, 10000);
     }
 
-    componentWillUnmount() {
+    componentWillUnmount()
+    {
         console.log("unmount"); // Not getting called.
         this.clearInterval(this.jobInterval);
     }
 
-    tryFetchJobList() {
+    tryFetchJobList()
+    {
         console.log("trying to fetch jobs...");
-        axios.get(`${api}/jobList`).then((response) => {
+        axios.get(`${api}/jobList`).then((response) =>
+        {
             console.log(response.data);
-            this.setState({jobList: response.data});
-        }).catch((err) => {
+            this.setState({ jobList: response.data });
+        }).catch((err) =>
+        {
             console.log(err);
         });
     }
 
-    render() {
+    goToJobDetails(job)
+    {
+        this.props.navigation.navigate('Job', {
+            jobType: job.jobType,
+            address: job.address,
+            author: job.author,
+            wage: job.wage,
+            description: job.description,
+            jobID: job.jobID,
+        });
+    }
+
+    render()
+    {
         return (
             <View style={styles.container}>
                 <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
                     <Text style={styles.headerText}>Job Board</Text>
-                    
+
                     <FlatList
                         style={styles.jobList}
                         data={this.state.jobList}
-                        renderItem={({item}) => (
-                            <TouchableOpacity style={styles.jobItem} onPress={() => alert("wain")}>
-                                <Text>Job type: {item.value[0].jobType}</Text>
-                                <Text>Posted by: {item.value[4].author}</Text>
-                                <Text>Address: {item.value[1].address}</Text>
-                                <Text>Wage: ${item.value[2].wage}</Text>
-                                <Text>Description: {item.value[3].description}</Text>
+                        renderItem={({ item }) => (
+                            <TouchableOpacity style={styles.jobItem} onPress={() => this.goToJobDetails(item.value)}>
+                                <Text>Job type: {item.value.jobType}</Text>
+                                <Text>Posted by: {item.value.author}</Text>
+                                <Text>Address: {item.value.address}</Text>
+                                <Text>Wage: ${item.value.wage}</Text>
+                                <Text>Description: {item.value.description}</Text>
+                                <Text>JobID: {item.value.jobID}</Text>
                             </TouchableOpacity>
                         )}
                     />
                 </ScrollView>
+                <View style={styles.tabBarInfoContainer}>
+                    <View style={[{ marginTop: 10, width: "70%" }]}>
+                        <Button
+                            title="Create a job"
+                            onPress={() => { this.props.navigation.navigate('CreateJob') }}
+                        />
+                    </View>
+                </View>
             </View>
         );
     };
@@ -99,5 +129,25 @@ const styles = StyleSheet.create({
         marginTop: 10,
         backgroundColor: "rgba(0,0,0,0.05)",
         padding: 30,
-    }
+    },
+    tabBarInfoContainer: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        ...Platform.select({
+          ios: {
+            shadowColor: 'black',
+            shadowOffset: { height: -3 },
+            shadowOpacity: 0.1,
+            shadowRadius: 3,
+          },
+          android: {
+            elevation: 20,
+          },
+        }),
+        alignItems: 'center',
+        backgroundColor: '#fbfbfb',
+        paddingVertical: 20,
+      },
 });
