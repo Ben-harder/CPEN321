c719d75272ff368785001e22e5e1e75e741ccf79
Ben <ben.harder2@gmail.com>
2018-10-25 00:34:33 -0700

    Changed URL to access remote backend

--- frontend/constants/Url.js
+++ frontend/constants/Url.js
@@ -2,7 +2,7 @@ import Expo from "expo";
 
 const { manifest } = Expo.Constants;
 const api = (typeof manifest.packagerOpts === `object`) && manifest.packagerOpts.dev
-  ? `http://${manifest.debuggerHost.split(`:`).shift().concat(`:3001`)}`
+  ? `http://40.118.188.95:8080` // `http://${manifest.debuggerHost.split(`:`).shift().concat(`:3001`)}`
   : `api.example.com`;
 
 export default api;
