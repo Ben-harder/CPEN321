3bd72fec869938868fc469a684ad612723f0d323
Osama Dawood <osama.k.dawood@gmail.com>
2018-11-18 14:16:24 -0800

    wained demo test for travis

---
+++ backend/test/demoTest.js
@@ -0,0 +1,13 @@
+const request = require('request');
+const assert = require('assert');
+const baseUrl = "http://localhost:3001";
+const expect = require('chai').expect;
+
+describe('Demo Tests', function(){
+
+  // demo test 1  
+  it('Demo test 1: (1 == 1)', function() {
+    expect(1).to.equal(1);
+  });
+
+});
