ab92c4e21a1bec9cbb71693aa31348055548b894
William Choi <wchoi95@gmail.com>
2018-11-23 15:35:35 -0800

    functionality of profile screen

--- frontend/screens/ProfileScreen.js
+++ frontend/screens/ProfileScreen.js
@@ -4,11 +4,16 @@ import
     Text,
     TouchableOpacity,
     View,
+    Image
 } from "react-native";
 import { connect } from "react-redux";
+import api from "../constants/Url";
+import axios from "axios";
 
 // styles
 const s = require('../constants/style');
+
+const placeholderImage = "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png";
 
 class Profile extends React.Component
 {
@@ -16,8 +21,41 @@ class Profile extends React.Component
     title: "My Profile",
   };
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      downVotes: 0,
+      upVotes: 0,
+      numOfTakenJobs: 0,
+      numOfPostedJobs: 0
+    };
+
+    this.getUserStats = this.getUserStats.bind(this);
+  }
+
   componentDidMount() {
-    
+    this.getUserStats();
+  }
+
+  getUserStats() {
+    const { user } = this.props;
+
+    axios.get(`${api}/user/get-user-profile`, {
+      params: {
+        userID: user.data.ID
+      }
+    }).then((res) => {
+      this.setState({
+        downVotes: res.data.downVotes,
+        upVotes: res.data.upVotes,
+        numOfTakenJobs: res.data.numOfTakenJobs,
+        numOfPostedJobs: res.data.numOfPostedJobs
+      });
+    }).catch((err) => {
+      console.log(err);
+      alert(err.response.data.errorMessage);
+    });
   }
 
   render() {
@@ -26,6 +64,9 @@ class Profile extends React.Component
     return (
       <View style={s.container}>
         <View style={[s.contentContainer, {flex: 1, justifyContent: 'center'}]}>
+          <View style={{alignItems: 'center'}}>
+            <Image source={{ uri: placeholderImage }} style={s.profilePicture} />
+          </View>
           <View style={{flexDirection: 'row', borderRadius: 10,}}>
             <Text style={[s.regTextBold, {textAlign: 'left'}]}>Name: </Text>
             <Text style={[s.regText, {textAlign: 'left'}]}>{user.data.firstName} {user.data.lastName}</Text>
@@ -34,6 +75,26 @@ class Profile extends React.Component
           <View style={{flexDirection: 'row', borderRadius: 10,}}>
             <Text style={[s.regTextBold, {textAlign: 'left'}]}>Phone #: </Text>
             <Text style={[s.regText, {textAlign: 'left'}]}>{user.data.phoneNumber}</Text>
+          </View>
+
+          <View style={{flexDirection: 'row', borderRadius: 10,}}>
+            <Text style={[s.regTextBold, {textAlign: 'left'}]}>Upvotes: </Text>
+            <Text style={[s.regText, {textAlign: 'left'}]}>{this.state.upVotes}</Text>
+          </View>
+
+          <View style={{flexDirection: 'row', borderRadius: 10,}}>
+            <Text style={[s.regTextBold, {textAlign: 'left'}]}>Downvotes: </Text>
+            <Text style={[s.regText, {textAlign: 'left'}]}>{this.state.downVotes}</Text>
+          </View>
+
+          <View style={{flexDirection: 'row', borderRadius: 10,}}>
+            <Text style={[s.regTextBold, {textAlign: 'left'}]}># of Taken Jobs: </Text>
+            <Text style={[s.regText, {textAlign: 'left'}]}>{this.state.numOfTakenJobs}</Text>
+          </View>
+
+          <View style={{flexDirection: 'row', borderRadius: 10,}}>
+            <Text style={[s.regTextBold, {textAlign: 'left'}]}># of Posted Jobs: </Text>
+            <Text style={[s.regText, {textAlign: 'left'}]}>{this.state.numOfPostedJobs}</Text>
           </View>
 
           <View>
