a7565742d5e3e9885f568d4692d5f7f5e3b2be1f
Ben <ben.harder2@gmail.com>
2018-11-23 14:06:12 -0800

    more address fixes

--- frontend/screens/AppliedJobsScreen.js
+++ frontend/screens/AppliedJobsScreen.js
@@ -98,9 +98,9 @@ class AppliedJobsScreen extends React.Component
                                     <Text style={s.jobTypeText}>{item.job_title}</Text>
                                     <Text style={[{fontSize: Font.titleSize,}]}>${item.wage}</Text>
                                 </View>
-                                <View style={{flexDirection: 'row', padding: 10,}}>
+                                <View style={{flexDirection: 'row', paddingVertical: 10,}}>
                                     <IOSIcon name="ios-compass" size={30} style={{color: Colors.sDark}}/>
-                                    <Text style={s.addressText}> {item.address}</Text> 
+                                    <Text style={s.addressText}> {item.address} </Text> 
                                 </View>
                                 <Text style={{fontSize: Font.smallSize}}>
                                     <Text style={{fontWeight: 'bold'}}>Posted by: </Text><Text>{item.employer.first_name} {item.employer.last_name}</Text>
--- frontend/screens/EmployerJobsScreen.js
+++ frontend/screens/EmployerJobsScreen.js
@@ -101,7 +101,7 @@ class EmployerJobsScreen extends React.Component
                                 </View>
                                 <View style={{flexDirection: 'row', paddingVertical: 10,}}>
                                     <IOSIcon name="ios-compass" size={30} style={{color: Colors.sDark}}/>
-                                    <Text style={s.addressText}> {item.address} </Text> 
+                                    <Text style= {s.addressText}> {item.address} </Text> 
                                 </View>
                                 <Text style={{fontSize: Font.smallSize}}>
                                     <Text style={{fontWeight: 'bold'}}>Posted by: </Text><Text>{item.employer.first_name} {item.employer.last_name}</Text>
