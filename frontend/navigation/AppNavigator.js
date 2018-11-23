5207a87fdf5d8b9be233b7884270855b57608b6d
Ben <ben.harder2@gmail.com>
2018-11-22 17:55:54 -0800

    updated header and colors

--- frontend/navigation/AppNavigator.js
+++ frontend/navigation/AppNavigator.js
@@ -18,15 +18,18 @@ import JobBoardScreen from "../screens/JobBoardScreen";
 import ProfileScreen from "../screens/ProfileScreen";
 import EditProfileScreen from "../screens/EditProfileScreen";
 import ChangePasswordScreen from "../screens/ChangePasswordScreen";
+import Colors from "../constants/Colors";
 
 const AppStack = createStackNavigator({ Main: JobBoardScreen, Job: JobScreen }, 
   {
     defaultNavigationOptions: ({navigation}) => ({
-      headerStyle: {marginRight: Platform.OS === 'ios' ? 10: 0, marginLeft: Platform.OS === 'ios' ? 15: 0, borderBottomWidth: 0,}
+      headerStyle: {marginRight: Platform.OS === 'ios' ? 10: 0, marginLeft: Platform.OS === 'ios' ? 15: 0, borderBottomWidth: 0,},
     }),
     navigationOptions: ({navigation}) => ({
+      headerTintColor: 'white',
+      headerStyle: {backgroundColor: Colors.sDark,},
       headerLeft:(<TouchableOpacity onPress={() => navigation.openDrawer()}>
-        <IOSIcon name="ios-menu" size={30} />
+        <IOSIcon name="ios-menu" size={30} style={{color: 'white',}}/>
         </TouchableOpacity>
       ),
       title: 'Job Board',
@@ -37,9 +40,11 @@ const AppStack = createStackNavigator({ Main: JobBoardScreen, Job: JobScreen },
 const CreateJobStack = createStackNavigator({CreateJob: CreateJobScreen},
   {
     navigationOptions: ({navigation}) => ({
+      headerTintColor: 'white',
+      headerStyle: {backgroundColor: Colors.sDark,},
       headerLeft:(<TouchableOpacity onPress={() => navigation.openDrawer()}>
-                    <IOSIcon name="ios-menu" size={30} />
-                  </TouchableOpacity>
+        <IOSIcon name="ios-menu" size={30} style={{color: 'white',}}/>
+        </TouchableOpacity>
       ),
       title: 'Create Job',
     })
@@ -48,9 +53,11 @@ const CreateJobStack = createStackNavigator({CreateJob: CreateJobScreen},
 const TakenJobsStack = createStackNavigator({TakenJobs: TakenJobsScreen},
   {
     navigationOptions: ({navigation}) => ({
+      headerTintColor: 'white',
+      headerStyle: {backgroundColor: Colors.sDark,},
       headerLeft:(<TouchableOpacity onPress={() => navigation.openDrawer()}>
-                    <IOSIcon name="ios-menu" size={30} />
-                  </TouchableOpacity>
+        <IOSIcon name="ios-menu" size={30} style={{color: 'white',}}/>
+        </TouchableOpacity>
       ),
       title: 'Taken Jobs',
     })
@@ -59,9 +66,11 @@ const TakenJobsStack = createStackNavigator({TakenJobs: TakenJobsScreen},
 const EmployerJobsStack = createStackNavigator({EmployerJobs: EmployerJobsScreen},
   {
     navigationOptions: ({navigation}) => ({
+      headerTintColor: 'white',
+      headerStyle: {backgroundColor: Colors.sDark,},
       headerLeft:(<TouchableOpacity onPress={() => navigation.openDrawer()}>
-                    <IOSIcon name="ios-menu" size={30} />
-                  </TouchableOpacity>
+        <IOSIcon name="ios-menu" size={30} style={{color: 'white',}}/>
+        </TouchableOpacity>
       ),
       title: 'Posted Jobs',
     })
@@ -71,9 +80,11 @@ const EmployerJobsStack = createStackNavigator({EmployerJobs: EmployerJobsScreen
 const ProfileStack = createStackNavigator({ProfileDetails: ProfileScreen, EditProfile: EditProfileScreen, ChangePassword: ChangePasswordScreen},
   {
     navigationOptions: ({navigation}) => ({
+      headerTintColor: 'white',
+      headerStyle: {backgroundColor: Colors.sDark,},
       headerLeft:(<TouchableOpacity onPress={() => navigation.openDrawer()}>
-                    <IOSIcon name="ios-menu" size={30} />
-                  </TouchableOpacity>
+        <IOSIcon name="ios-menu" size={30} style={{color: 'white',}}/>
+        </TouchableOpacity>
       ),
     })
   }
