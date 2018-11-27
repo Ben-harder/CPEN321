f089b4db4824688dcd8bc23ad67b901150a7139f
Osama Dawood <osama.k.dawood@gmail.com>
2018-11-27 14:51:57 -0800

    hmm2

--- admin/index.js
+++ admin/index.js
@@ -2,6 +2,21 @@ var http = require('http');
 var fs = require('fs');
 
 const PORT=8080;
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 fs.readFile('./index.html', function (err, html) {
     if (err) throw err;
